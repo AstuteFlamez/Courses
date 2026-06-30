1418e3ec3c189e643fa1325f161fb850d28c9ee8
Bilal Usman <busman4@wisc.edu>
2026-06-30 16:01:14 -0500

    Added Header Comment and .gitignore

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+/**
+    * Main class for CS400 A03.VersionControl activity Spring - 2025
+    * @author: Bilal Usman, busman4
+**/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Why is beef stew a bad password?");
-	    System.out.println("<punchline>");
+	    System.out.println("It isn't stew!");
     }
 }
