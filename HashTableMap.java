b6e6734e57e489d7a1405c5ad2f72699068e010c
Bilal Usman <busman4@wisc.edu>
2026-07-31 16:03:00 -0500

    cs400 script submission from Fri Jul 31 04:03:00 PM CDT 2026

--- HashTableMap.java
+++ HashTableMap.java
@@ -6,31 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class HashTableMap<KeyType, ValueType> implements MapADT<KeyType, ValueType> {
-
+ 
     /**
      * Inner class used to store a key, value pair together inside 
      * a LinkedList chain within the hashtable array.
      */
     protected class Pair {
-
+ 
         public KeyType key;
         public ValueType value;
-
+ 
         public Pair(KeyType key, ValueType value) {
             this.key = key;
             this.value = value;
         }
-
+ 
     }
-
+ 
     // The single array field used to store our key, value pairs.
     // Each index of the array holds a LinkedList (chain) of Pair objects
     // that all hashed to that index.
     protected LinkedList<Pair>[] table = null;
-
+ 
     // Keeps track of the number of key, value pairs currently stored.
     private int size = 0;
-
+ 
+    // Load factor threshold at (or above) which the table is resized.
+    private static final double MAX_LOAD_FACTOR = 0.75;
+ 
     /**
      * Constructs a new HashTableMap with the specified initial capacity.
      * @param capacity the initial size of the underlying array
@@ -40,85 +43,185 @@ public class HashTableMap<KeyType, ValueType> implements MapADT<KeyType, ValueTy
         this.table = (LinkedList<Pair>[]) new LinkedList[capacity];
         this.size = 0;
     }
-
+ 
     /**
      * Constructs a new HashTableMap with a default capacity of 64.
      */
     public HashTableMap() {
         this(64);
     }
-
+ 
+    /**
+     * Computes the index within the table array that a given key hashes to,
+     * based on the current capacity of the table.
+     * @param key the key to hash
+     * @return the index within the current table array
+     */
+    private int hashIndex(KeyType key) {
+        return Math.abs(key.hashCode()) % table.length;
+    }
+ 
+    /**
+     * Doubles the capacity of the underlying array and rehashes every
+     * existing key, value pair into the new, larger array.
+     */
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        LinkedList<Pair>[] oldTable = this.table;
+        int newCapacity = oldTable.length * 2;
+        LinkedList<Pair>[] newTable = (LinkedList<Pair>[]) new LinkedList[newCapacity];
+ 
+        for (int i = 0; i < oldTable.length; i++) {
+            LinkedList<Pair> chain = oldTable[i];
+            if (chain != null) {
+                for (Pair pair : chain) {
+                    int newIndex = Math.abs(pair.key.hashCode()) % newCapacity;
+                    if (newTable[newIndex] == null) {
+                        newTable[newIndex] = new LinkedList<Pair>();
+                    }
+                    newTable[newIndex].add(pair);
+                }
+            }
+        }
+ 
+        this.table = newTable;
+    }
+ 
     /**
      * Adds a new key,value pair/mapping to this collection.
      */
     @Override
     public void put(KeyType key, ValueType value) throws IllegalArgumentException {
-        // TODO
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null.");
+        }
+        if (containsKey(key)) {
+            throw new IllegalArgumentException("Key is already mapped to a value.");
+        }
+ 
+        // Grow the table first if adding this pair would push the load
+        // factor to or above the threshold.
+        double loadFactorAfterAdd = (double) (size + 1) / table.length;
+        if (loadFactorAfterAdd >= MAX_LOAD_FACTOR) {
+            resize();
+        }
+ 
+        int index = hashIndex(key);
+        if (table[index] == null) {
+            table[index] = new LinkedList<Pair>();
+        }
+        table[index].add(new Pair(key, value));
+        size++;
     }
-
+ 
     /**
      * Checks whether a key maps to a value in this collection.
      */
     @Override
     public boolean containsKey(KeyType key) {
-        // TODO
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null.");
+        }
+        int index = hashIndex(key);
+        LinkedList<Pair> chain = table[index];
+        if (chain == null) {
+            return false;
+        }
+        for (Pair pair : chain) {
+            if (pair.key.equals(key)) {
+                return true;
+            }
+        }
         return false;
     }
-
+ 
     /**
      * Retrieves the specific value that a key maps to.
      */
     @Override
     public ValueType get(KeyType key) throws NoSuchElementException {
-        // TODO
-        return null;
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null.");
+        }
+        int index = hashIndex(key);
+        LinkedList<Pair> chain = table[index];
+        if (chain != null) {
+            for (Pair pair : chain) {
+                if (pair.key.equals(key)) {
+                    return pair.value;
+                }
+            }
+        }
+        throw new NoSuchElementException("Key not found in this collection.");
     }
-
+ 
     /**
      * Remove the mapping for a key from this collection.
      */
     @Override
     public ValueType remove(KeyType key) throws NoSuchElementException {
-        // TODO
-        return null;
+        if (key == null) {
+            throw new NullPointerException("Key cannot be null.");
+        }
+        int index = hashIndex(key);
+        LinkedList<Pair> chain = table[index];
+        if (chain != null) {
+            for (Pair pair : chain) {
+                if (pair.key.equals(key)) {
+                    chain.remove(pair);
+                    size--;
+                    return pair.value;
+                }
+            }
+        }
+        throw new NoSuchElementException("Key not found in this collection.");
     }
-
+ 
     /**
      * Removes all key, value pairs from this collection without changing the
      * capacity of the underlying array.
      */
     @Override
     public void clear() {
-        // TODO
+        for (int i = 0; i < table.length; i++) {
+            table[i] = null;
+        }
+        size = 0;
     }
-
+ 
     /**
      * Retrieves the number of keys stored in this collection.
      */
     @Override
     public int getSize() {
-        // TODO
-        return 0;
+        return size;
     }
-
+ 
     /**
      * Retrieves this collection's capacity.
      */
     @Override
     public int getCapacity() {
-        // TODO
-        return 0;
+        return table.length;
     }
-
+ 
     /**
      * Retrieves this collection's keys.
      */
     @Override
     public List<KeyType> getKeys() {
-        // TODO
-        return null;
+        LinkedList<KeyType> keys = new LinkedList<KeyType>();
+        for (int i = 0; i < table.length; i++) {
+            LinkedList<Pair> chain = table[i];
+            if (chain != null) {
+                for (Pair pair : chain) {
+                    keys.add(pair.key);
+                }
+            }
+        }
+        return keys;
     }
-
+ 
     // JUnit 5 test method
 
     /**
